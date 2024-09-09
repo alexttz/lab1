07ef17ee9a6e323cc7306523d268c1e0c89502c7
Alexander Silva Lima <111926533+alexttz@users.noreply.github.com>
2024-09-09 10:23:53 -0300

    Delete exercicios directory

--- exercicios/ex05/SalarioVendas/src/CalculoSalario.java
+++
@@ -1,7 +0,0 @@
-public class CalculoSalario
- {
-    public static void main(String[] args) throws Exception {
-        Funcionario funcionario1 = new Funcionario(5, 500, 5000);
-        funcionario1.mostrarResultado();
-    }
-}
--- exercicios/ex06/FarenCelsius/src/ConversorTemperatura.java
+++
@@ -1,9 +0,0 @@
-
-// Classe principal para executar o programa
-public class ConversorTemperatura {
-    public static void main(String[] args) {
-        // Exemplo de uso
-        Temperatura temp = new Temperatura(212);
-        temp.exibirCelsius();
-    }
-}
